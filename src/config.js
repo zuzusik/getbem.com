ec5fa181ad702458dbc161815f2d00c582e616a6
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-12-06 22:46:35 +0500

    Example of index page

--- src/config.js
+++ src/config.js
@@ -1,14 +1,21 @@
 System.config({
   "paths": {
     "*": "*.js",
-    "~/*": "lib/*.js",
+    "~/*": "*.js",
     "github:*": "jspm_packages/github/*.js"
   }
 });
 
 System.config({
   "map": {
-    "jquery": "github:components/jquery@2.1.1"
+    "bootstrap": "github:twbs/bootstrap@3.3.1",
+    "css": "github:systemjs/plugin-css@0.1.0",
+    "github-buttons": "github:component/github-buttons@0.0.4",
+    "jquery": "github:components/jquery@2.1.1",
+    "github:twbs/bootstrap@3.3.1": {
+      "css": "github:systemjs/plugin-css@0.1.0",
+      "jquery": "github:components/jquery@2.1.1"
+    }
   }
 });
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-var $ = require('jquery');
-
-$('body').html('Hello from JS!');
+require('bootstrap');
+require('github-buttons');
+require('github-buttons/style.css!'); // Temporary hack to include styles
+require('~/blocks/twitter');
+require('~/blocks/advantage.css!');
+require('~/blocks/footer.css!');
+require('~/blocks/header.css!');
+require('~/blocks/consumers.css!');
