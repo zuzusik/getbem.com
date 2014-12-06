1b528032c1bc8efe63bd961188e0d2300720826d
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-12-06 23:02:25 +0500

    Move imports to HTML

--- src/index.js
+++
@@ -1,8 +0,0 @@
-require('bootstrap');
-require('github-buttons');
-require('github-buttons/style.css!'); // Temporary hack to include styles
-require('~/blocks/twitter');
-require('~/blocks/advantage.css!');
-require('~/blocks/footer.css!');
-require('~/blocks/header.css!');
-require('~/blocks/consumers.css!');
