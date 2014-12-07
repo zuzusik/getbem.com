0d417cad48e4a5455df95261b17853cca0083d8c
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-12-07 19:22:05 +0500

    Add introduction with affix
     * config.js now contains absolute paths

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 System.config({
   "paths": {
-    "*": "*.js",
-    "github:*": "jspm_packages/github/*.js"
+    "*": "/*.js",
+    "github:*": "/jspm_packages/github/*.js"
   }
 });
 
---
+++ src/index.js
@@ -0,0 +1,11 @@
+require('bootstrap');
+require('github-buttons');
+require('github-buttons/style.css!'); // Temporary hack to include styles
+require('blocks/twitter');
+require('blocks/advantage.css!');
+require('blocks/affix.css!');
+require('blocks/footer.css!');
+require('blocks/header.css!');
+require('blocks/consumers.css!');
+
+module.exports = require('jquery');
