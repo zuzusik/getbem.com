c90806eb911f2fccc86e612ebe284868169fff1c
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-12-09 22:43:28 +0500

    Fixing container

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,6 @@
 require('bootstrap');
 require('blocks/header.css!');
 
-/*
-require('blocks/twitter');
-require('blocks/advantages/advantages.css!');
-require('blocks/affix.css!');
-require('blocks/footer.css!');
-
-require('blocks/consumers.css!');*/
-
 var React = require('react');
 var NavBar = require('blocks/navbar/navbar.jsx!');
 var Footer = require('blocks/footer/footer.jsx!');
@@ -22,8 +14,10 @@ React.render(
             <img src="/images/b.svg"  className="header__logo"/>
             <h1 className="header__desc">BEM — Block Element Modifier is a methodology, that helps you to achieve reusable components and code sharing in the front-end.</h1>
         </div>
-        <Advantages/>
-        <Consumers/>
+        <div className="container">
+            <Advantages/>
+            <Consumers/>
+        </div>
         <Footer/>
     </div>,
     document.body
